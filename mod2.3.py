fb47eafd12a5fea5a736d67831eaf024add614bd
sallywalks <joolney21@gmail.com>
2025-04-21 00:50:20 -0400

    2-3
    IT-140 SNHU

---
+++ mod2.3.py
@@ -0,0 +1,12 @@
+#***********************#
+#JoAnn Olney           *#
+#09-15-2024            *#
+#IT-140                *#
+#mod2-3                *#
+#***********************#
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+print('Hello', name, "you were born in", 2024-age)
+
