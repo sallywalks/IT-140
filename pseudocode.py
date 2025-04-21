e96fa6c317092ff7ba430fa01462be49a75edfa8
sallywalks <joolney21@gmail.com>
2025-04-21 00:49:18 -0400

    Pseudocode
    IT-140 SNHU

---
+++ pseudocode.py
@@ -0,0 +1,31 @@
+JoAnn Olney
+9-22-2024
+IT-140
+Modulde 3-3
+Pseudocode
+
+This program will request the workers hours from the user. If the hours are greater or equal to 40 they earn $20 an hour.
+If the employee workers 60 hours they will earn $30 an hour for the 20 extra hours they work.
+
+Input the number of hours worked
+
+Declare the variable hours and pay
+    Weekly pay
+    Overtime hours
+    Overtime pay
+
+IF the employees hours worked is hours less than or equal to 40:
+   THEN Weekly pay = number times 20:
+        PRINT"Weekly Paycheck"
+ELSE:
+    Overtime hours (60<= 40):
+    Number of hours worked minus 40:
+    Overtime hours - Number of hours worked:
+    Overtime pay = Overtime hours times $30:
+    Total weekly paycheck = Weekly pay + Overtime pay:
+        PRINT"Overtime Paycheck"
+
+
+
+
+
